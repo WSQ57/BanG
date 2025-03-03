0be085d1aaf5337db48fa01fad315b1848488dc2
wsq <wsq5457@outlook.com>
2025-03-03 21:16:43 +0800

    学习正则来校验

--- webook/internal/web/user.go
+++ webook/internal/web/user.go
@@ -4,11 +4,30 @@ import (
 	"fmt"
 	"net/http"
 
+	regexp "github.com/dlclark/regexp2"
+
 	"github.com/gin-gonic/gin"
 )
 
 // 定义User相关路由
 type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	const (
+		emailRegexp    = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$"
+		passwordRegexp = "^(?=.*\\d)(?=.*[A-z])[\\da-zA-Z]{1,9}$"
+	)
+
+	emailExp := regexp.MustCompile(emailRegexp, regexp.None)
+	passwordExp := regexp.MustCompile(passwordRegexp, regexp.None)
+
+	return &UserHandler{
+		emailExp:    emailExp,
+		passwordExp: passwordExp,
+	}
 }
 
 func (u *UserHandler) RegisterRoutesv1(ug *gin.RouterGroup) {
@@ -17,7 +36,7 @@ func (u *UserHandler) RegisterRoutesv1(ug *gin.RouterGroup) {
 	ug.POST("signup", u.Signup)
 	ug.POST("login", u.Login)
 	ug.POST("edit", u.Edit)
-	ug.GET("profil", u.Profile)
+	ug.GET("profile", u.Profile)
 }
 
 func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
@@ -30,8 +49,9 @@ func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
 }
 
 func (u *UserHandler) Signup(ctx *gin.Context) {
+	// 内部结构体，不给别人用
 	type SignUpReq struct {
-		Email           string `json:"email"`
+		Email           string `json:"email"` // 对应json中的email字段
 		ConfirmPassWord string `json:"ConfirmPassWord"`
 		Password        string `json:"password"`
 	}
@@ -42,6 +62,34 @@ func (u *UserHandler) Signup(ctx *gin.Context) {
 	if err := ctx.Bind(&req); err != nil {
 		fmt.Println("asdasd")
 	}
+
+	// 使用正则表达式校验正则表达式
+	ok, err := u.emailExp.MatchString(req.Email)
+
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		ctx.String(http.StatusOK, "邮箱格式不正确")
+		return
+	}
+
+	ok, err = u.passwordExp.MatchString(req.Password)
+	if err != nil {
+		ctx.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		ctx.String(http.StatusOK, "密码小于9位，且由数字字母组成")
+		return
+	}
+
+	if req.Password != req.ConfirmPassWord {
+		ctx.String(http.StatusOK, "两次密码不一致")
+		return
+	}
+
 	ctx.String(http.StatusOK, "注册成功")
 	fmt.Printf("%v", req)
 }
--- webook/main.go
+++ webook/main.go
@@ -9,14 +9,16 @@ import (
 
 func main() {
 	server := gin.Default()
-	u := &web.UserHandler{}
-	// u.RegisterRoutes(server)
+	// u := &web.UserHandler{}
+
+	u := web.NewUserHandler()
 
 	// 配置 CORS
 	server.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"http://localhost:3000"},         // 允许的跨域源
-		AllowMethods: []string{"GET", "POST", "OPTIONS"},        // 允许的 HTTP 方法
-		AllowHeaders: []string{"Content-Type", "Authorization"}, // 允许的请求头
+		AllowOrigins:     []string{"http://localhost:3000"},         // 允许的跨域源
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},        // 允许的 HTTP 方法
+		AllowHeaders:     []string{"Content-Type", "Authorization"}, // 允许的请求头
+		AllowCredentials: true,
 	}))
 
 	u.RegisterRoutesv1(server.Group("/users"))
